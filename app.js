68c939423dd4d71bcd16460ab6a06859a96ebd62
iFlameing <ialokkumarsingh0@gmail.com>
2019-07-23 00:51:44 +0530

    adding intial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require ('body-parser');
+const twitterWebhooks = require('twitter-webhooks');
+
+const app = express();
+app.use(bodyParser.json());
+
+const userActivityWebhook = twitterWebhooks.userActivity({
+    serverUrl: 'https://iflameingtwitter-bot.herokuapp.com',
+    route: '/', //default : '/'
+    consumerKey: process.env.consumerKey,
+    consumerSecret: process.env.consumerSecret,
+    accessToken: process.env.accessToken,
+    accessTokenSecret: process.env.accessTokenSecret,
+    environment: process.env.environment, //default : 'env-beta'
+    app
+});
+
+//Register your webhook url - just needed once per URL
+userActivityWebhook.register();
+
+//Subscribe for a particular user activity
+userActivityWebhook.subscribe({
+    userId: process.env.USERNAME,
+    accessToken: process.env.accessToken,
+    accessTokenSecret: process.env.accessTokenSecret,
+})
+.then(function (userActivity) {
+    userActivity
+    .on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
+    .on ('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
+    .on ('follow', (data) => console.log (userActivity.id + ' - follow'))
+    .on ('mute', (data) => console.log (userActivity.id + ' - mute'))
+    .on ('revoke', (data) => console.log (userActivity.id + ' - revoke'))
+    .on ('direct_message', (data) => console.log (userActivity.id + ' - direct_message'))
+    .on ('direct_message_indicate_typing', (data) => console.log (userActivity.id + ' - direct_message_indicate_typing'))
+    .on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
+    .on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
+});
+
+//listen to any user activity
+userActivityWebhook.on ('event', (event, userId, data) => console.log (userId + ' - favorite'));
+
+//listen to unknown payload (in case of api new features)
+userActivityWebhook.on ('unknown-event', (rawData) => console.log (rawData));
+
+app.listen(process.env.PORT || 2345, () => console.log("server is started on 2345"))
